55f7145201e47021521952f541946b5102eaf837
deads2k <deads@redhat.com>
2016-11-08 15:09:05 -0500

    add basic validators

--- pkg/api/validation/validation.go
+++ pkg/api/validation/validation.go
@@ -1,24 +1,62 @@
 package validation
 
 import (
+	"fmt"
+	"net"
+
 	"k8s.io/kubernetes/pkg/api/validation"
+	"k8s.io/kubernetes/pkg/api/validation/path"
+	utilvalidation "k8s.io/kubernetes/pkg/util/validation"
 	"k8s.io/kubernetes/pkg/util/validation/field"
 
 	discoveryapi "github.com/openshift/kube-aggregator/pkg/api"
 )
 
-func ValidateAPIServerName(name string, prefix bool) []string {
-	if reasons := validation.ValidateNamespaceName(name, false); len(reasons) != 0 {
-		return reasons
+func ValidateAPIServer(apiServer *discoveryapi.APIServer) field.ErrorList {
+	requiredName := apiServer.Spec.Version + "." + apiServer.Spec.Group
+
+	allErrs := validation.ValidateObjectMeta(&apiServer.ObjectMeta, false,
+		func(name string, prefix bool) []string {
+			if minimalFailures := path.IsValidPathSegmentName(name); len(minimalFailures) > 0 {
+				return minimalFailures
+			}
+			// the name *must* be version.group
+			if name != requiredName {
+				return []string{fmt.Sprintf("must be `spec.version+\".\"+spec.group`: %q", requiredName)}
+			}
+
+			return []string{}
+		},
+		field.NewPath("metadata"))
+
+	// in this case we all empty group
+	if len(apiServer.Spec.Group) == 0 && apiServer.Spec.Version != "v1" {
+		allErrs = append(allErrs, field.Required(field.NewPath("spec", "group"), "only v1 may have an empty group and it better be legacy kube"))
+	}
+	if len(apiServer.Spec.Group) > 0 {
+		for _, errString := range utilvalidation.IsDNS1123Subdomain(apiServer.Spec.Group) {
+			allErrs = append(allErrs, field.Invalid(field.NewPath("spec", "group"), apiServer.Spec.Group, errString))
+		}
 	}
 
-	return nil
-}
+	for _, errString := range utilvalidation.IsDNS1035Label(apiServer.Spec.Version) {
+		allErrs = append(allErrs, field.Invalid(field.NewPath("spec", "version"), apiServer.Spec.Version, errString))
+	}
 
-func ValidateAPIServer(apiServer *discoveryapi.APIServer) field.ErrorList {
-	result := validation.ValidateObjectMeta(&apiServer.ObjectMeta, false, ValidateAPIServerName, field.NewPath("metadata"))
+	if apiServer.Spec.Priority <= 0 {
+		allErrs = append(allErrs, field.Invalid(field.NewPath("spec", "priority"), apiServer.Spec.Priority, "priority must be positive"))
 
-	return result
+	}
+
+	if len(apiServer.Spec.InternalHost) == 0 {
+		allErrs = append(allErrs, field.Required(field.NewPath("spec", "internalHost"), ""))
+	} else {
+		if _, _, err := net.SplitHostPort(apiServer.Spec.InternalHost); err != nil {
+			allErrs = append(allErrs, field.Invalid(field.NewPath("spec", "internalHost"), apiServer.Spec.InternalHost, err.Error()))
+		}
+	}
+
+	return allErrs
 }
 
 func ValidateAPIServerUpdate(newAPIServer *discoveryapi.APIServer, oldAPIServer *discoveryapi.APIServer) field.ErrorList {
